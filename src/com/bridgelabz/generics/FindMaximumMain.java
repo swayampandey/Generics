638b4f686ea998c09c4506db32971ca452e72611
swayampandey <swayam265@gmail.com>
2022-08-28 12:41:18 +0530

    [Swayam] : added functionality of find maximum in given integers

---
+++ src/com/bridgelabz/generics/FindMaximumMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.generics;
+
+public class FindMaximumMain {
+
+	public static void main(String[] args) {
+		
+		FindMaximum maximum = new FindMaximum();
+		System.out.println("Maximumn in given integers is :");
+		maximum.checkMaximum(75, 42, 112);
+	}
+
+}
